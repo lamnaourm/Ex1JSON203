98ba578c0e88e60fe02acf1a7ec17365b37f77a0
lamnaourm <mohammed.lamnaour@gmail.com>
2022-06-04 14:15:05 +0100

    Traiter un objet JSON.

--- app/src/main/java/com/example/ex1json203/MainActivity.java
+++ app/src/main/java/com/example/ex1json203/MainActivity.java
@@ -3,15 +3,54 @@ package com.example.ex1json203;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tnom, tage, tfiliere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tnom = findViewById(R.id.tnom);
+        tage = findViewById(R.id.tage);
+        tfiliere = findViewById(R.id.tfil);
+
+        String json = loadJsonFromRaw(R.raw.stagiaire);
+
+        try {
+            JSONObject obj = new JSONObject(json);
+
+            tnom.setText("Nom : " + obj.getString("nom"));
+            tage.setText("age : " + obj.getInt("age"));
+            tfiliere.setText("Filiere : " + obj.getString("filiere"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
+    // Lire fichier
+    public String loadJsonFromRaw(int resId){
+        try {
+            InputStream in = getResources().openRawResource(resId);
+            byte[] data = new byte[in.available()];
+            in.read(data);
+            in.close();
+            return new String(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
